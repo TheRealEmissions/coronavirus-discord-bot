ef870bffdaaf967d29713e89ad020fe43c3c05b2
Emissions <unitedrealm@hotmail.com>
2020-04-02 20:32:33 +0100

    Updated guildDelete.js (event)

--- src/events/guildDelete.js
+++ src/events/guildDelete.js
@@ -1,10 +1,12 @@
 module.exports = async (head, client, guild) => {
+   let color = typeof guild.me == "undefined" ? null : guild.me.displayHexColor;
+   if (!color) color = null;
    head.log(`Left guild: ${guild.name} (${guild.id})`);
    head.log(`Members: ${guild.memberCount}`);
    let channel = await head.functions.getLogChannel(head, client);
    channel.send(
       new head.modules.Discord.MessageEmbed()
-         .setColor(guild.me.displayHexColor)
+         .setColor(color)
          .setTitle(`**Left Guild:**`)
          .addField(`Name:`, `${guild.name}\n*${guild.id}*`, true)
          .addField(`Membercount:`, guild.memberCount, true)
