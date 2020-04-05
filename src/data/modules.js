c3ed11b96aa41220550283428ca636af7420fd1b
Emissions <unitedrealm@hotmail.com>
2020-04-05 02:38:11 +0100

    Updated modules.js (data)

--- src/data/modules.js
+++ src/data/modules.js
@@ -1,4 +1,3 @@
-const { auth } = require(`../../index`);
 module.exports = {
    Discord: require(`discord.js`),
    fs: require(`fs`),
@@ -6,7 +5,7 @@ module.exports = {
    djs_commands: require(`djs-commands`),
    request: require(`request-promise-native`),
    himalaya: require(`himalaya`),
-   plotly: require(`plotly`)("Emissions", auth.plotly_key),
+   plotly: require(`plotly`)("Emissions", require(`./auth`).plotly_key),
    axios: require(`axios`),
    // npm i --save TheRealEmissions/djs-commands
 };
