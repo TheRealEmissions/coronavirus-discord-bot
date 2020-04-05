1200c39d58804414fd09c87862de21d3d58ba60d
Emissions <unitedrealm@hotmail.com>
2020-04-05 03:00:43 +0100

    Updated web_http.js (controller)

--- src/controller/web_http.js
+++ src/controller/web_http.js
@@ -2,29 +2,38 @@ class botsondiscord {
    constructor(head, client) {
       this.head = head;
       this.client = client;
-      this.init();
+      this.init().catch((err) => head.error(err));
+      setInterval(() => {
+         this.init().catch((err) => head.error(err));
+      }, 300000);
    }
 
    guildCount() {
       return new Promise(async (resolve, reject) => {
          const res = await this.head.modules.axios
             .post(
-               `https://bots.ondiscord.xyz/bot-api/bots/${this.head.auth.bod_api_key}/guilds`,
+               `https://bots.ondiscord.xyz/bot-api/bots/${this.client.user.id}/guilds`,
                {
                   guildCount: this.client.guilds.cache.array().length,
+               },
+               {
+                  headers: {
+                     Authorization: this.head.auth.bod_api_key,
+                     "Content-Type": "application/json",
+                  },
                }
             )
             .catch((err) => reject(err));
          resolve();
          this.head.log(`bots.ondiscord.xyz response: ${res.statusCode}`);
-         this.head.post(res);
+         this.head.post(JSON.stringify(res));
          return;
       });
    }
 
    init() {
       return new Promise(async (resolve, reject) => {
-         await this.guildCount();
+         await this.guildCount().catch((err) => reject(err));
          return resolve();
       });
    }
