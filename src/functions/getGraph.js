1365aa7bf40f9a6d7c289a641cda74eb77437008
Emissions <unitedrealm@hotmail.com>
2020-04-07 00:14:19 +0100

    Updated getGraph.js (function)

--- src/functions/getGraph.js
+++ src/functions/getGraph.js
@@ -2,11 +2,11 @@ module.exports = (head, region) => {
    return new Promise((resolve, reject) => {
       head.models.graphs.findOne(
          {
-            region: region
+            region: region,
          },
          (err, db) => {
             if (err) return reject(err);
-            if (!db) return reject(`No graph for region provided`);
+            if (!db) return resolve("none");
             return resolve(db.url);
          }
       );
